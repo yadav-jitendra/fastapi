3cbeb0654fab7055030f4a3fb2e2fd63140ad1ed
Jitendra Yadav <j.yadav@tu-braunschweig.de>
2024-08-16 20:51:56 +0200

    update

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import List
-from uuid import uuid4
+from uuid import UUID, uuid4
 from fastapi import FastAPI
 
 from models import Gender, Role, User
@@ -8,7 +8,7 @@ app = FastAPI()
 
 db: List[User] = [
     User(
-        id=uuid4(), 
+        id=UUID("c80e23e8-0518-4cb5-88c4-251edd0e0e2d"), 
         first_name="Jitendra",
         middle_name="Kumar",
         last_name="Yadav",
@@ -18,10 +18,10 @@ db: List[User] = [
     User(
         id=uuid4(), 
         first_name="Joe",
-        middle_name="von",
+        # middle_name="von",
         last_name="Doe",
         gender=Gender.male,
-        roles=[Role.student]
+        roles=[Role.student, Role.user]
     )
 ]
 
@@ -30,5 +30,17 @@ async def root():
     return {"Hello": "Fastapi"}
 
 @app.get("/api/v1/users")
-async def ftech_users():
+async def fetch_users():
     return db
+
+@app.post("/api/v1/users")
+async def register_user(user: User):
+    db.append(user)
+    return {"id": user.id}
+
+@app.delete("/api/v1/users/{user_id}")
+async def delete_user(user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            db.remove(user)
+            return
